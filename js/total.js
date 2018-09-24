655493c2b9ae0178ed307f212794b56e5d4ea4c3
mortanbor <42118390+mortanbor@users.noreply.github.com>
2018-09-24 22:28:07 +0200

    Проверка 2 внесенные правки

--- js/total.js
+++ js/total.js
@@ -10,64 +10,49 @@ var email = popup.querySelector("[name=e-mail]");
 var isStorageSupport = true;
 var storage = "";
   
-  try {
-    storage = localStorage.getItem("login");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-    link.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.add("modal-show");
-
-	if (storage) {
-      login.value = storage;
-      email.focus();
-    } else {
-      login.focus();
-    }
-  });
-
-    close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-  });
-
-    form.addEventListener("submit", function (evt) {
-    if (!login.value || !email.value) {
-      evt.preventDefault();
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem("login", login.value);
-      }
-    }
-  });
-
-     window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-      }
-    }
-  });
-
-
-     /*карта*/
-
-     function initMap() {
-  var mapOptions = {
-    zoom: 17,
-    center: new google.maps.LatLng(59.9387165,30.3208587)
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-                                mapOptions);
-  var image = "../img/decor/map-marker.png";
-  var myLatLng = new google.maps.LatLng(59.9387165,30.3208587);
-  var beachMarker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: image
-  });
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+
+  link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
+if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
+});
+
+  close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+  form.addEventListener("submit", function (evt) {
+if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
+});
+
+  window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+    evt.preventDefault();
+  if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
